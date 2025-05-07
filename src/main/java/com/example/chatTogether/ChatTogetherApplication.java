9efee21b01ebe47e4675622a33f200b0693e0b27
ankitkumar <ankitkumar5049@gmail.com>
2025-05-07 16:21:13 +0530

    Initial commit

---
+++ src/main/java/com/example/chatTogether/ChatTogetherApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatTogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatTogetherApplication.class, args);
+	}
+
+}
